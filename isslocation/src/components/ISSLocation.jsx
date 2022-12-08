65c19000b2b859573795fcfa5ca88a0bbabad8f0
Ben-Hur Carvalho <benhurcarvalho2@gmail.com>
2022-12-07 22:27:14 -0300

    aplica pigeon-maps com sucesso

--- isslocation/src/components/ISSLocation.jsx
+++ isslocation/src/components/ISSLocation.jsx
@@ -1,9 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Map, { Marker } from 'pigeon-maps';
 
 class ISSLocation extends Component {
   render() {
     return (
-      <div>ISSLocation</div>
+      <main>
+        <div className='map'>
+            <Map
+                center={ [0,0] }
+                defaultWidth= { 450 }
+                height={ 450 }
+                minZoom={ 1.5 }
+                maxZoom={ 50 }
+                zoom={ 1.5 }
+            />
+
+        </div>
+      </main>
     )
   }
 }

f7650c5f215832eb0772799582962a7973dd9b97
Ben-Hur Carvalho <benhurcarvalho2@gmail.com>
2022-12-07 22:01:17 -0300

    importa para o projeto img svg

--- isslocation/src/components/Header.jsx
+++ isslocation/src/components/Header.jsx
@@ -3,7 +3,9 @@ import React, { Component } from 'react'
 class Header extends Component {
   render() {
     return (
-      <div>Header</div>
+      <div className='header'>
+        <h1>Space Station Tracker</h1>
+      </div>
     )
   }
 }

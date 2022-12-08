c14fd2b472911dd0bc4a443310f63c0e54d93b47
Ben-Hur Carvalho <benhurcarvalho2@gmail.com>
2022-12-07 23:59:10 -0300

    monta footer com links de referencia para icon

--- isslocation/src/App.js
+++ isslocation/src/App.js
@@ -4,10 +4,15 @@ import ISSLocation from './components/ISSLocation';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <ISSLocation />
-    </div>
+    <>
+      <div className="App">
+        <Header />
+        <ISSLocation />
+      </div>
+      <footer className='footer'>
+        <a target="blank" href="https://icons8.com/icon/2VXZb3OKn14e/satellite">Satellite icon by Icons8</a>
+      </footer>
+    </>
   );
 }
 

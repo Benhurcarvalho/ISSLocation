9878fa61bffc6e117c0f8341259ec9f82b28cdaa
Ben-Hur Carvalho <benhurcarvalho2@gmail.com>
2022-12-07 22:10:50 -0300

    prepara e estrutura header

--- isslocation/src/components/Header.jsx
+++ isslocation/src/components/Header.jsx
@@ -1,10 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import latImg from '../assets/latitude.svg';
+import lonImg from '../assets/longitude.svg';
 
 class Header extends Component {
   render() {
+    const latitude = 0;
+    const longitude = 0;
     return (
       <div className='header'>
         <h1>Space Station Tracker</h1>
+        <h1>
+            <img src={ latImg } alt='' width='30' />
+            { `Latitude: ${ latitude }`}
+        </h1>
+        <h1>
+            {' '}
+            <img src={ lonImg } alt="" width='30' />
+            { `Longitude: ${ longitude }`}
+        </h1>
       </div>
     )
   }

b7d8826433f758d88eae78833d9182142d62044d
Ben-Hur Carvalho <benhurcarvalho2@gmail.com>
2022-12-07 21:55:48 -0300

    cria componentes Header e ISSLocation

---
+++ isslocation/src/components/ISSLocation.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class ISSLocation extends Component {
+  render() {
+    return (
+      <div>ISSLocation</div>
+    )
+  }
+}
+
+export default ISSLocation;
